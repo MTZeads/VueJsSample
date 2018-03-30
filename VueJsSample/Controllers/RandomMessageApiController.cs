8e24efec6b7dbdc276a61aaf1f0b835c1a1b7e39
Sébastien Hodgson <sebastien.hodgson@outlook.com>
2018-03-30 15:03:59 -0400

    init

---
+++ VueJsSample/Controllers/RandomMessageApiController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Web.Http;
+
+namespace VueJsSample.Controllers
+{
+    public class RandomMessageApiController : ApiController
+    {
+        [Route("api/RandomMessageAPI/Get")]
+        [HttpGet]
+        public string Get()
+        {
+            return $"API returned {new Random().Next(1, 1000)}";
+        }
+    }
+}
